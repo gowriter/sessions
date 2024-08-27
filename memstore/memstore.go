6756405f2c4763466f7e9f2c2ed54283dcdc332d
Stefan Brem <3203564+gowriter@users.noreply.github.com>
2024-08-27 19:35:50 +0700

    feat: add memstore

---
+++ memstore/memstore.go
@@ -0,0 +1,44 @@
+package memstore
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gowriter/session"
+)
+
+type MemoryStore struct {
+	Store map[string]any
+}
+
+func NewMemoryStore() session.Store {
+	return &MemoryStore{Store: map[string]any{}}
+}
+
+func (m *MemoryStore) New(_ context.Context, sessionID string, data any) error {
+	m.Store[sessionID] = data
+	return nil
+}
+
+func (m *MemoryStore) Get(_ context.Context, sessionID string) (json.RawMessage, error) {
+	data, ok := m.Store[sessionID]
+	if !ok {
+		return nil, session.ErrNotFound
+	}
+
+	b, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func (m *MemoryStore) Put(_ context.Context, sessionID string, session any) error {
+	m.Store[sessionID] = session
+	return nil
+}
+
+func (m *MemoryStore) End(_ context.Context, sessionID string) error {
+	delete(m.Store, sessionID)
+	return nil
+}
---
+++ memstore/memstore_test.go
@@ -0,0 +1,237 @@
+package memstore
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gowriter/session"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type TestType struct {
+	ID   int
+	Name string
+}
+
+func TestMemoryStore_Get(t *testing.T) {
+	type fields struct {
+		Store session.Store
+	}
+	type args struct {
+		ctx       context.Context
+		sessionID string
+	}
+
+	ctx := context.Background()
+	testSessionData := TestType{
+		ID:   1,
+		Name: "test",
+	}
+
+	testSessionJSON, err := json.Marshal(testSessionData)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name        string
+		sessionJSON []byte
+		fields      fields
+		args        args
+		want        json.RawMessage
+		wantErr     error
+	}{
+		{
+			name: "session exists",
+			fields: fields{
+				Store: NewMemoryStore(),
+			},
+			args: args{
+				ctx:       ctx,
+				sessionID: "test_session_id",
+			},
+			want: testSessionJSON,
+		},
+		{
+			name: "error session not exists",
+			fields: fields{
+				Store: NewMemoryStore(),
+			},
+			args: args{
+				ctx:       ctx,
+				sessionID: "wrong_test_session_id",
+			},
+			wantErr: session.ErrNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.fields.Store.(*MemoryStore)
+			s.Store["test_session_id"] = testSessionData
+
+			got, err := tt.fields.Store.Get(tt.args.ctx, tt.args.sessionID)
+			if tt.wantErr != nil {
+				assert.EqualError(t, err, tt.wantErr.Error())
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Get() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMemoryStore_Put(t *testing.T) {
+	type fields struct {
+		Store session.Store
+	}
+	type args struct {
+		ctx         context.Context
+		sessionID   string
+		sessionData TestType
+	}
+
+	ctx := context.Background()
+
+	tests := []struct {
+		name        string
+		sessionJSON []byte
+		fields      fields
+		args        args
+		wantErr     bool
+	}{
+		{
+			name: "success put value",
+			fields: fields{
+				Store: NewMemoryStore(),
+			},
+			args: args{
+				ctx:       ctx,
+				sessionID: "test_session_id",
+				sessionData: TestType{
+					ID:   1,
+					Name: "test",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.fields.Store.(*MemoryStore)
+			s.Store[tt.args.sessionID] = TestType{}
+
+			err := s.Put(tt.args.ctx, tt.args.sessionID, tt.args.sessionData)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Put() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantErr {
+				assert.EqualError(t, err, session.ErrNotFound.Error())
+			}
+			assert.NoError(t, err)
+			got, ok := s.Store[tt.args.sessionID]
+			assert.True(t, ok)
+			assert.Equal(t, tt.args.sessionData, got)
+		})
+	}
+}
+
+func TestMemoryStore_End(t *testing.T) {
+	type fields struct {
+		Store session.Store
+	}
+	type args struct {
+		ctx       context.Context
+		sessionID string
+	}
+
+	ctx := context.Background()
+
+	tests := []struct {
+		name        string
+		sessionJSON []byte
+		fields      fields
+		args        args
+		wantErr     bool
+	}{
+		{
+			name: "success end session",
+			fields: fields{
+				Store: NewMemoryStore(),
+			},
+			args: args{
+				ctx:       ctx,
+				sessionID: "test_session_id",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.fields.Store.(*MemoryStore)
+			s.Store[tt.args.sessionID] = TestType{}
+
+			err := s.End(tt.args.ctx, tt.args.sessionID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("End() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantErr {
+				assert.EqualError(t, err, session.ErrNotFound.Error())
+			}
+			assert.NoError(t, err)
+			_, ok := s.Store[tt.args.sessionID]
+			assert.False(t, ok)
+		})
+	}
+}
+
+func TestMemoryStore_New(t *testing.T) {
+	type fields struct {
+		Store session.Store
+	}
+	type args struct {
+		ctx         context.Context
+		sessionID   string
+		sessionData TestType
+	}
+
+	ctx := context.Background()
+
+	tests := []struct {
+		name        string
+		sessionJSON []byte
+		fields      fields
+		args        args
+		wantErr     bool
+	}{
+		{
+			name: "success new session",
+			fields: fields{
+				Store: NewMemoryStore(),
+			},
+			args: args{
+				ctx:       ctx,
+				sessionID: "test_session_id",
+				sessionData: TestType{
+					ID:   1,
+					Name: "test",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.fields.Store.(*MemoryStore)
+
+			err := s.New(tt.args.ctx, tt.args.sessionID, tt.args.sessionData)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantErr {
+				assert.EqualError(t, err, session.ErrNotFound.Error())
+			}
+			assert.NoError(t, err)
+			got, ok := s.Store[tt.args.sessionID]
+			assert.True(t, ok)
+			assert.Equal(t, tt.args.sessionData, got)
+		})
+	}
+}
